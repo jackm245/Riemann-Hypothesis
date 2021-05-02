5f272a0d28bcb2f50b7f06ac38f68852f3cd8a8b
Jack Morgan <jack.morgan2@challoners.org>
2021-05-02 19:23:01 +0100

    Create zeta_by_definition.py

---
+++ Prototypes/Zeta Function/zeta_by_definition.py
@@ -0,0 +1,30 @@
+#=============================================================================#
+#
+#    File:   zeta_by_definition.py
+#    Author: Jack Morgan
+#    Date:   May 2021
+#    Description:
+#        Computes the zeta function for Re(s) > 1
+#        where \zeta(s)=\sum _{n=0}^{\infty }\frac{1}{n^s}
+#
+#=============================================================================#
+
+
+# calculates zeta(s) for any complex number where Re(s) > 1
+# where \zeta(s)=\sum _{n=0}^{\infty }\frac{1}{n^s}
+def zeta(s: complex) -> complex:
+    # validate that Re(s) > 1
+    if not s.real > 1:
+        raise ValueError('Real part of input must be greater than 1')
+    TERMS = 1 * 10**6 # the number of terms that we wish to compute.
+    res = 0
+    # computes an approximation for the infinite sum
+    for n in range(1, TERMS+1):
+        res += 1/(n**s)
+    # output the final result
+    return res
+
+
+for x in range(2,10):
+    for y in range(2,10):
+        print(zeta(x + y * 1j))
