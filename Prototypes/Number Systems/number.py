ae6601b9f5f65086525dbed118b00b46b273119b
Jack Morgan <jack.morgan2@challoners.org>
2021-05-05 22:33:19 +0100

    Update number.py

--- Prototypes/Number Systems/number.py
+++ Prototypes/Number Systems/number.py
@@ -1,3 +1,14 @@
+#=============================================================================#
+#
+#    File:   number.py
+#    Author: Jack Morgan
+#    Date:   May 2021
+#    Description:
+#        Contains the class Number
+#        Number class inherited by other number system classes
+#
+#=============================================================================#
+
 class Number:
 
     def __init__(self, number):
