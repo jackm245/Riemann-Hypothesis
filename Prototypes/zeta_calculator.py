75f8cf83b6bd85e00d94b2d0fa355041c974362d
Jack Morgan <jack.morgan2@challoners.org>
2021-05-17 16:38:55 +0100

    Create zeta_calculator.py

---
+++ Prototypes/zeta_calculator.py
@@ -0,0 +1,12 @@
+#=============================================================================#
+#
+#    File:   zeta_calculator.py
+#    Author: Jack Morgan
+#    Date:   May 2021
+#    Description:
+#        The user will input a value into the zeta calculator
+#        The program will output the result
+#        A step-by-step solution will be shown
+#
+#=============================================================================#
+
